5d633677081809655aa821f38c65cc239a7915ac
mohammadfrh <left.inter@gmail.com>
2020-02-15 12:44:18 +0330

    test

---
+++ searchlibrary/src/main/java/com/frh/searchlibrary/SimpleOnSearchActionListener.java
@@ -0,0 +1,23 @@
+package com.frh.searchlibrary;
+
+/**
+ * Created by mancj on 26.01.17.
+ */
+
+public abstract class SimpleOnSearchActionListener implements MaterialSearchBar.OnSearchActionListener {
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
+    }
+}
