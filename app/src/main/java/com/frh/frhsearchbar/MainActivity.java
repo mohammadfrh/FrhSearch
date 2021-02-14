ce669aac59927cd1ade387df73383069a776299e
Mohamad <m.farahzadi@vasl.ir>
2021-02-14 12:42:20 +0330

    start cuddle

--- app/src/main/java/com/frh/frhsearchbar/MainActivity.java
+++ app/src/main/java/com/frh/frhsearchbar/MainActivity.java
@@ -11,10 +11,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -22,6 +27,7 @@ import android.widget.Toast;
 import com.frh.searchlibrary.MaterialSearchBar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> result;
     LinearLayout linearItem;
     RelativeLayout searchview;
+    EditText editText;
+    String[] test2;
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -40,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
         search_bar = findViewById(R.id.search_bar);
         linearItem = findViewById(R.id.linear_item);
         searchview = findViewById(R.id.searchview);
-        search_bar.setHint("جستجو ...");
+        search_bar.setHint("search malls");
         search_bar.setRoundedSearchBarEnabled(true);
-
+        editText = search_bar.findViewById(R.id.mt_editText);
         search_bar.mtSearchBlack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,45 +59,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         test = new ArrayList<>();
         result = new ArrayList();
-
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
-        test.add("دفتر پیشخوان دولت");
+        test2 = new String[]{"Westlake Center", "tehran", "qom", "arak", "esfehan", "soed", "mashhad", "esfehan"};
 
 
-        final ConstraintLayout constraintLayout = (ConstraintLayout) search_bar.findViewById(R.id.root);
-        final RecyclerView recyclerView = (RecyclerView) search_bar.findViewById(R.id.mt_recycler);
-        recyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        test.add("Westlake Center");
+        test.add("tehran");
+        test.add("qom");
+        test.add("qom1");
+        test.add("qom2");
+        test.add("qom3");
+        test.add("arak");
+        test.add("esfehan");
+        test.add("soed");
+        test.add("mashhad");
+        test.add("esfehan");
+
+        editText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onGlobalLayout() {
-                
-                // constraintLayout.setMaxHeight(600);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
-        });
 
-        for (int i = 0; i <= 5; i++) {
-            result.add(test.get(i));
-            search_bar.setLastSuggestions(result);
-        }
-
-        linearItem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if(search_bar.isSuggestionsVisible()) {
-                    search_bar.hideSuggestionsList();
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                for (int i = 0; i < test.size(); i++) {
+                    if (test.get(i).contains(s.toString().trim())) {
+                        result.add(test.get(i));
+                        search_bar.setLastSuggestions(result);
+                        search_bar.showSuggestionsList();
+                    } else {
+                        result.remove(test.get(i));
+                       // search_bar.hideSuggestionsList();
+                    }
+
+
                 }
-                else
-                    search_bar.showSuggestionsList();
             }
         });
+
     }
 }
